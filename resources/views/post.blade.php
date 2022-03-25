adbdd14e0e30a3c6056efa63e7ba35fd1bb9a186
BSIT-AZUR <201910554@gordoncollege.edu.ph>
2022-03-26 01:20:33 +0800

    LARAVEL-MIDTERM-EXAM

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master') 
+@section('title', 'Post') 
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+    <h1>HELLO</h1>
+</div>
+</div>
+</div>
+@endsection
